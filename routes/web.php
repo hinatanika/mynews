74984321784b0643d3d2ebbfff8c72eb4e7af123
hinata3030@icloud.com <ec2-user@ip-172-31-1-82.ap-southeast-2.compute.internal>
2023-12-15 16:59:05 +0000

    mynews

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/profile/create', 'Admin\ProfileController@add');
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+
+
+Route::get('XXX', 'AAAController@bbb');
